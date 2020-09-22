71059f1edf19dbdc7094192d6c17a20c9b7a4d69
Maxim Schiriy <maximschiriy@MacBook-Pro-Maxim.local>
2020-09-22 16:21:46 +0300

    unuse isDate from lodash

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,8 @@
-import isDate from 'lodash/isDate';
 import makeApolloFormQuery from './query';
+
+function isDate(v: any) {
+   return Object.prototype.toString.call(v) === '[object Date]';
+}
 
 function replaceValues(target: any, source: any, value: any) {
    for (const key of Object.keys(source)) {
