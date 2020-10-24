0f5cd5eff2d06647dcc3c5bce0c9d873c5ae2f4e
Maxim Schiriy <maximschiriy@MacBook-Pro-Maxim.local>
2020-10-24 23:05:40 +0300

    update peers

---
+++ docs/main.3257cf7174f5f7e679b6.bundle.js
@@ -0,0 +1,10 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+   [0],
+   {
+      421: function(n, o, p) {
+         p(422), p(565), (n.exports = p(1244));
+      },
+      484: function(n, o) {},
+   },
+   [[421, 1, 2]],
+]);
