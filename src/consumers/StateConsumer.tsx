f900eb1e6eada96ca71feef6e2e78ea1a1c65221
Maxim Schiriy <maximschiriy@MacBook-Pro-Maxim.local>
2020-12-21 22:31:09 +0300

    update StateConsumer

--- src/consumers/StateConsumer.tsx
+++ src/consumers/StateConsumer.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import useApolloFormCtx from '../hooks/useApolloFormCtx';
 import { ApolloFormState } from '../types';
+import FormManager from '../managers/FormManager';
 
-export interface StateListenerProps<V> {
-   children: React.ComponentType<{ state: V }>;
+export interface StateListenerProps<V extends object> {
+   children: React.ComponentType<{ state: V; form: FormManager<V> }>;
    selector?: (state: ApolloFormState<any>) => V;
 }
 
-function StateConsumer<V = any>({ children: Render, selector }: StateListenerProps<V>) {
-   const apolloForm = useApolloFormCtx<any>();
+function StateConsumer<V extends object = {}>({
+   children: Render,
+   selector,
+}: StateListenerProps<V>) {
+   const apolloForm = useApolloFormCtx<V>();
    const state = apolloForm.useState<V>(selector);
 
-   return <Render state={state} />;
+   return <Render state={state} form={apolloForm} />;
 }
 
 export default StateConsumer;
